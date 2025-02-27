76ee54ba0d4624ce07d51ef992cbb2b366a49c3e
Ugur Emre Dogan <ue.dogan@gmail.com>
2025-02-26 19:41:58 -0800

    Added user model.

--- src/app/main.py
+++ src/app/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
-from datetime import datetime, UTC
 
 from app.core.database import Base, engine
-from sqlalchemy import Column, Integer, Boolean, String, DateTime
-from pydantic import BaseModel, EmailStr, Field, constr, field_validator
+from pydantic import BaseModel, EmailStr, constr
 from app.routes.user import router as user_router
 
 Base.metadata.create_all(bind=engine)
@@ -16,17 +14,7 @@ def read_root():
     return {"Hello": "World"}
 
 
-class User(Base):
-    __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password_hash = Column(String)
-    is_active = Column(Boolean, default=True)
-    is_verified = Column(Boolean, default=False)
-    verification_code = Column(String, nullable=True)
-    created_at = Column(DateTime, default=datetime.now(UTC))
 
 
 class UserBase(BaseModel):
---
+++ src/app/models/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, Boolean, String, DateTime
+from datetime import datetime, UTC
+from app.core.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password_hash = Column(String)
+    is_active = Column(Boolean, default=True)
+    is_verified = Column(Boolean, default=False)
+    verification_code = Column(String, nullable=True)
+    created_at = Column(DateTime, default=datetime.now(UTC))
+
+    # TODO: Add relations to transactions and categories
