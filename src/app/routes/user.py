889453bab0af21a84ab039ef4222d5dbeea1c9e4
Ugur Emre Dogan <ue.dogan@gmail.com>
2025-02-26 19:37:42 -0800

    Added ruff and bandit dependencies

--- src/app/routes/user.py
+++ src/app/routes/user.py
@@ -11,3 +11,7 @@ def register_user():
 @router.post("/token")
 async def login_for_access_token():
     return {"message": "User logged in successfully"}
+
+
+def example_func():
+    return "Hello from example_func"
