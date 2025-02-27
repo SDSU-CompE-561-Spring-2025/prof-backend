ba048d952707b553d1c49833e2f90c2c592e8e96
Ugur Emre Dogan <ue.dogan@gmail.com>
2025-02-26 20:30:04 -0800

    Updated the user service to support new authentication logic.

--- src/app/services/user.py
+++ src/app/services/user.py
@@ -1,14 +1,24 @@
 from app.core.config import get_settings
+from app.schemas.user import UserCreate
+from sqlalchemy.orm import Session
+from app.core.auth import get_password_hash
+from app.models.user import User
 
 settings = get_settings()
 
 
 # User CRUD operations
-def create_user():
-    print("User created successfully")
-    return {
-        "id": 1,
-        "username": "SrJ1SQPRXLbRN_9vAFAedQrTV4NwmH0BGtCVr5A1QmARS1AxRX",
-        "email": "user@example.com",
-        "created_at": "2025-02-27T03:55:53.783Z",
-    }
+def create_user(db: Session, user: UserCreate):
+    hashed_password = get_password_hash(user.password)
+    verification_code = "1234"  # TODO: Implement verification code
+
+    db_user = User(
+        username=user.username,
+        email=user.email,
+        password_hash=hashed_password,
+        verification_code=verification_code,
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
