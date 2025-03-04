b25bf336ac7d3d25fcb7ed8da9b5e56aadcdf6a0
Ugur Emre Dogan <ue.dogan@gmail.com>
2025-03-03 19:35:01 -0800

    Created api for login and register.

--- src/app/routes/user.py
+++ src/app/routes/user.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 
+from app.core.auth import ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token
+from app.schemas.token import Token
 from app.schemas.user import UserResponse, UserCreate
 import app.services.user as user_service
 from app.dependencies import get_db
 from sqlalchemy.orm import Session
+from fastapi import HTTPException, status
+from datetime import timedelta
 
 
 router = APIRouter()
@@ -15,9 +20,26 @@ def register_user(user: UserCreate, db: Session = Depends(get_db)):
     return new_user
 
 
-@router.post("/token")
-async def login_for_access_token():
-    return {"message": "User logged in successfully"}
+@router.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+):
+    user = user_service.authenticate_user(
+        db, username=form_data.username, password=form_data.password
+    )
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 @router.get("/users/me")
@@ -28,7 +50,3 @@ def read_users_me():
 @router.post("/users/verify-email/{verification_code}")
 def verify_email(verification_code: str):
     return {"message": "Email verified successfully"}
-
-
-def example_func():
-    return "Hello from example_func"
--- src/app/services/user.py
+++ src/app/services/user.py
@@ -1,4 +1,5 @@
 from app.core.config import get_settings
+from app.core.security import verify_password
 from app.schemas.user import UserCreate
 from sqlalchemy.orm import Session
 from app.core.auth import get_password_hash
@@ -22,3 +23,12 @@ def create_user(db: Session, user: UserCreate):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
+def authenticate_user(db: Session, username: str, password: str):
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        return False
+    if not verify_password(password, user.password_hash):
+        return False
+    return user
